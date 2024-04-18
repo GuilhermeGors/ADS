2905604ff99ec1bfa355476d56d2d316c1040d2b
Guilherme Gomes de Oliveira <45400708+GuilhermeGors@users.noreply.github.com>
2024-04-18 08:44:52 -0300

    Add files via upload

---
+++ Lab1/ex_semana1_pedido_endereco.py
@@ -0,0 +1,11 @@
+
+# ex_semana1_pedido_endereco
+
+print("Registering address: ")
+number          = int(input("Enter your house number: "))
+road            = input("Enter your road: ")
+neighborhood    = input("Enter your neighborhood: ")
+city            = input("Enter your city: ")
+state           = input("Enter your state: ")
+
+print(f"This is your adress: \nNumber: {number}\nRoa: {road}\nNeighborhood: {neighborhood} \nCity: {city} \nState: {state}")
