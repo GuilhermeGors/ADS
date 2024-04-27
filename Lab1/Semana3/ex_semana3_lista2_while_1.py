02f6346c83a3f38a19bda36fcd991d5105059372
Guilherme Gomes de Oliveira <guilhermegors@gmail.com>
2024-04-27 18:56:20 -0300

    Exercicios

---
+++ Lab1/Semana3/ex_semana3_lista2_while_1.py
@@ -0,0 +1,16 @@
+
+
+count =0
+
+while count != 10:
+
+     num = int((input("Enter a random number: ")))
+     count += 1
+     if num<0:
+      print("NUMERO NEGATIVO")
+     elif num ==0:
+      print("zero")
+     else:
+      print("NUMERO POSITIVO")        
+
+print("Fim")
---
+++ Lab1/Semana3/ex_semana3_lista2_while_2.py
@@ -0,0 +1,16 @@
+
+
+count =0
+
+num1 = int(input("Enter the first value: "))
+num2 = int(input("Enter the second value: "))
+
+if num1 > num2:
+     print(f"First value:{num1} cannot be greater than Second value:{num2}!")
+else:
+
+      while num1 != num2:
+
+            num1 += 1
+            print(num1)
+
